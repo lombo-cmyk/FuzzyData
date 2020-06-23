6116acee99a753472e918f66fdc02ef614c994db
lkania <lukasz.kania.cc@gmail.com>
2020-06-23 17:15:27 +0200

    Add centers to plots, clustering to varying no dimensions

--- mymain.py
+++ mymain.py
@@ -18,7 +18,9 @@ def load_data():
     train_data = np.delete(train_data, slice(1, 16), 1)
     test_data = np.genfromtxt('ann-test.data', delimiter=' ')
     test_data = np.delete(test_data, slice(1, 16), 1)
-    return train_data, test_data
+    return train_data[:, :-1], test_data[:, :-1], train_data[:, -1], \
+           test_data[:, -1]
+
 
 def create_directories():
     script_dir = os.path.dirname(__file__)
@@ -30,13 +32,14 @@ def create_directories():
     os.makedirs(current_working_dir)
     return current_working_dir
 
-def perform_PCA(training: np.array, test: np.array):
-    pca = PCA(n_components=2)
+
+def perform_PCA(training: np.array, test: np.array, no_componenets=2):
+    pca = PCA(n_components=no_componenets)
     pca_ret = list()
     for dataset in [training, test]:
         pca.fit(dataset)
         pca_ret.append(pca.transform(dataset))
-    return pca_ret[0], pca_ret[1]
+    return pca_ret
 
 
 def add_diagnosis_info(arr: np.array, diagnosis: np.array):
@@ -46,11 +49,15 @@ def add_diagnosis_info(arr: np.array, diagnosis: np.array):
     return ret
 
 
-def plot_results(data: np.array, title: type.__str__, dir, no_clusters=3):
+def plot_results(data: np.array, title: type.__str__, dir, no_clusters=3,
+                 n_dim=2, centers=None):
     for clust in range(no_clusters, 0, -1):
         plt.plot(data[data[:, 2] == clust, 0], data[data[:, 2] == clust, 1],
                  'o', markersize=3)
-    plt.title(title)
+    if centers is not plot_results.__defaults__[2]:
+        for pt in centers:
+            plt.plot(pt[0], pt[1], 'rs')
+    plt.title(title + ", dimensions: "+ str(n_dim))
     plt.savefig(dir + title)
     plt.close()
 
@@ -65,7 +72,7 @@ def perform_cmeans(train_set: np.array, test_set: np.array,
     test_clusters += 1
     train_clusters = np.argmax(train_labels, 0)
     train_clusters += 1
-    return train_clusters.T, test_clusters.T
+    return train_clusters.T, test_clusters.T, center
 
 
 def perform_kmeans(train_set: np.array, test_set: np.array,
@@ -79,35 +86,39 @@ def perform_kmeans(train_set: np.array, test_set: np.array,
 
 def main():
     current_working_dir = create_directories()
-    train_data, test_data = load_data()
-
-    train_2d, test_2d = perform_PCA(train_data[:, :-1], test_data[:, :-1])
-    train_2d_with_diagnosis = add_diagnosis_info(train_2d, train_data[:, -1])
-    test_2d_with_diagnosis = add_diagnosis_info(test_2d, test_data[:, -1])
+    init_train_data, init_test_data, init_train_diag, init_test_diag = load_data()
+    no_dimensions = 2
+    train_2d, test_2d = perform_PCA(init_train_data[:, :-1],
+                                    init_test_data[:, :-1])
+    train_data, test_data = perform_PCA(init_train_data[:, :-1],
+                                    init_test_data[:, :-1], no_dimensions)
+    train_2d_with_diagnosis = add_diagnosis_info(train_2d, init_train_diag)
+    test_2d_with_diagnosis = add_diagnosis_info(test_2d, init_test_diag)
     plot_results(train_2d_with_diagnosis, "training dataset - given classes",
                  current_working_dir)
     plot_results(test_2d_with_diagnosis, "test dataset - given classes",
                  current_working_dir)
-
-    train_clusters, test_clusters = perform_cmeans(train_data[:, :-1],
-                                                   test_data[:, :-1])
+    a, a, center = perform_cmeans(train_2d, test_2d)
+    train_clusters, test_clusters, a = perform_cmeans(train_data, test_data)
     train_data_cmeans_diagnosis = add_diagnosis_info(train_2d, train_clusters)
     test_data_cmeans_diagnosis = add_diagnosis_info(test_2d, test_clusters)
     plot_results(train_data_cmeans_diagnosis,  "training dataset fuzzy "
                                                "clustering results",
-                 current_working_dir)
+                 current_working_dir, n_dim=no_dimensions, centers=center)
     plot_results(test_data_cmeans_diagnosis, "test dataset - fuzzy clustering "
-                                             "results", current_working_dir)
+                                             "results", current_working_dir,
+                 n_dim=no_dimensions, centers=center)
 
-    train_clusters, test_clusters = perform_kmeans(train_data[:, :-1],
-                                                   test_data[:, :-1])
+    train_clusters, test_clusters = perform_kmeans(train_data,
+                                                   test_data)
     train_data_kmeans_diagnosis = add_diagnosis_info(train_2d, train_clusters)
     test_data_kmeans_diagnosis = add_diagnosis_info(test_2d, test_clusters)
     plot_results(train_data_kmeans_diagnosis, "training dataset crisp "
                                               "clustering results",
-                 current_working_dir)
+                 current_working_dir, n_dim=no_dimensions, centers=center)
     plot_results(test_data_kmeans_diagnosis, "test dataset crisp clustering "
-                                             "results", current_working_dir)
+                                             "results", current_working_dir,
+                 n_dim=no_dimensions, centers=center)
 
 
 if __name__ == "__main__":
